2abb21c1be96b7e64763a066fbcd1d67e365b897
riddhivk <riddhivkoli@gmail.com>
2024-12-31 14:22:19 +0530

    riddhis 4th commit: bg is full screen and logo image added.

--- frontend/my-app/src/components/Header.jsx
+++ frontend/my-app/src/components/Header.jsx
@@ -30,8 +30,10 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <img src="/WhatsApp Image 2024-12-25 at 15.00.41_ebc78587.jpg" alt="Routify Logo" className="logo-image" /> {/* Adjust the path as necessary */}
-        <Link to="/">Routify</Link>
+      <img src="/routify_logo_final.png" alt="Routify Logo" className="logo-image" />
+
+      {/* Adjust the path as necessary */}
+      <Link to="/">Routify</Link>
       </div>
       <nav className="nav">
         <Link to="/">Home</Link>
